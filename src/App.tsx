e467f39fedf665140789ae6249c47276ba71672e
Jordan <jordanroberts333@icloud.com>
2023-06-11 14:46:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="flex">
+       <div className="bg-blue-400">sidebar</div>
+       <div className="bg-blue-500 flex-1"></div>
+    </div>
+
+  );
+}
+
+export default App;
